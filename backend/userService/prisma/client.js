4976a1482f49a7a07e43479b60983292ebb0c23c
HieuTrungMc <phantrunghieu1109@gmail.com>
2025-04-27 21:02:26 +0700

    fix: remove all js error files

--- backend/userService/prisma/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-// Create a singleton instance of PrismaClient
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
