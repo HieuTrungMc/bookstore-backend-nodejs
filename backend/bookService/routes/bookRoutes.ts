16ced85538aff1c33858e64b43e3c5d74647f953
Lan Anh <dhlananh2309@gmail.com>
2025-04-28 23:15:33 +0700

    feat: add new getCategoryById functionality

--- backend/bookService/routes/bookRoutes.ts
+++ backend/bookService/routes/bookRoutes.ts
@@ -7,19 +7,21 @@ import {
     fetchAllBooks, fetchAllCategories, fetchBookDetails, fetchBooksByCategory, fetchNewArrivals, fetchRecommendations,
     searchBooks,
     updateBookById,
-    updateCategoryById
+    updateCategoryById,
+    getCategoryById
 } from "../controllers/bookController";
 
 const router = express.Router();
 
 router.get('/all', fetchAllBooks);
 router.get('/allcategory', fetchAllCategories)
-router.post('/createbook',createNewBook)
+router.post('/createbook', createNewBook)
 router.post('/createcategory', createNewCategory)
 router.post('/deletebook/:bookId', deleteBookById)
 router.post('/updatebook/:bookId', updateBookById)
 router.post('/updatecategory/:categoryId', updateCategoryById)
-router.post('/deletecategory/:categoryId',deleteCategoryById)
+router.post('/category/:categoryId', updateCategoryById)
+router.post('/deletecategory/:categoryId', deleteCategoryById)
 router.get('/category/:categorySlug', fetchBooksByCategory);
 router.get('/new-arrivals/:categorySlug', fetchNewArrivals);
 router.get('/recommendations/:categorySlug', fetchRecommendations);
